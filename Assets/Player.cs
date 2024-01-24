150644aae5d217eba0b2fe15c8c74216012241e9
ArchanaSharikalHarinarayanan <archana.s.h@saveetha.ac.in>
2024-01-24 12:30:34 +0530

    First commit

---
+++ Assets/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    void start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // type the code here
+    }
+}
